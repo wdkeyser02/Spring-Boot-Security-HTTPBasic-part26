217a3ad9a4b744a17c9518af17e9bcf0e7bf1e6c
Willy De Keyser <spring.boot.programming@gmail.com>
2026-05-14 09:45:34 +0200

    Init

---
+++ Spring-Boot-Security-HTTPBasic-part26/src/main/java/willydekeyser/SpringBootSecurityHttpBasicPart26Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityHttpBasicPart26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityHttpBasicPart26Application.class, args);
+	}
+
+}
